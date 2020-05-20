95131a417fd08fbe2e4eb0e56e2c7912821af48d
xiaoran <ran.xiao@tendcloud.com>
2020-05-20 13:46:33 +0800

    打家劫舍1  动态规划入门经典

---
+++ src/main/java/algorithm/dp/Steal.java
@@ -0,0 +1,46 @@
+package algorithm.dp;
+
+/**
+ * leetcode 198. 打家劫舍
+ *
+ *
+ * https://leetcode-cn.com/problems/house-robber/
+ *
+ * 输入: [2,7,9,3,1]
+ * 输出: 12
+ * 解释: 偷窃 1 号房屋 (金额 = 2), 偷窃 3 号房屋 (金额 = 9)，接着偷窃 5 号房屋 (金额 = 1)。
+ *      偷窃到的最高金额 = 2 + 9 + 1 = 12 。
+ *
+ *
+ * @author xiaoran
+ * @date 2020/05/19
+ */
+public class Steal {
+
+    public static void main(String[] args) {
+        Steal steal = new Steal();
+        int[] nums = {2,1,1,2};
+        System.out.println(steal.rob(nums));
+    }
+
+    public int rob(int[] nums) {
+        //一些特殊的条件
+        if(nums == null || nums.length<=0){
+            return 0;
+        }
+
+        //定义dp数组
+        //dp[i] 代表到达第 i 个房屋偷得的最高金额，也就是当前最大子序和
+        int[] dp = new int[nums.length+1];
+
+        //初始化的值
+        dp[0] = 0;
+        dp[1] = nums[0];
+        //确定元素间的动态关系
+        for(int i=2;i<=nums.length;i++){
+            //当前房间偷与不偷， 两种结果最优值对比
+            dp[i] = Math.max((nums[i-1] + dp[i-2]),dp[i-1]);
+        }
+        return dp[nums.length];
+    }
+}
