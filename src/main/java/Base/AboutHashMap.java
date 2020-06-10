fcd8de5266ad7f52677e2b22dcc58405fc7d88f9
xiaoran <ran.xiao@tendcloud.com>
2020-06-10 11:19:23 +0800

    HashMap和ConcurrentHashMap 存储key-value为null的数据

--- src/main/java/Base/AboutHashMap.java
+++ src/main/java/Base/AboutHashMap.java
@@ -2,7 +2,9 @@ package Base;
 
 import java.lang.reflect.Method;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * @author xiaoran
@@ -22,5 +24,21 @@ public class AboutHashMap {
         Method capacity = mapType.getDeclaredMethod("capacity");
         capacity.setAccessible(true);
         System.out.println("capacity : " + capacity.invoke(map));
+
+
+        //HashMap 可以存储key-value 为null的数据
+        Map<String,String> map1  = new HashMap<>();
+        map1.put(null,"123");
+        map1.put("123",null);
+
+        map1.entrySet().forEach(System.out::println);
+
+
+        //ConcurrentHashMap 不存储key-value 为null的数据
+        Map<String,String> concurrentMap = new ConcurrentHashMap<>();
+        concurrentMap.put(null,"123");
+        concurrentMap.put("123",null);
+
+        concurrentMap.entrySet().forEach(System.out::println);
     }
 }
