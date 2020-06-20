858b4fb407ad9ab44c0095f18eb412215e7fa62f
xiaoran <ran.xiao@tendcloud.com>
2020-06-20 17:01:51 +0800

    二分查找的非递归版本

--- src/main/java/algorithm/binarySearch/BinarySearch.java
+++ src/main/java/algorithm/binarySearch/BinarySearch.java
@@ -10,6 +10,9 @@ public class BinarySearch {
         BinarySearch binarySearch = new BinarySearch();
         int index = binarySearch.binarySearch(new int[]{-1,0,3,5,9,12},0,6,9);
         System.out.println(index);
+
+
+        System.out.println(binarySearch.binarySearch(new int[]{-1,0,3,5,9,12},9));
     }
 
     /**
@@ -34,5 +37,28 @@ public class BinarySearch {
     }
 
 
+    /**
+     * 二分查找的非递归版本
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int binarySearch(int[] nums,int target){
+        int low = 0;
+        int high = nums.length-1;
+        while(high > low){
+            int mid = (low+high)/2;
+            if(target == nums[mid]){
+                return mid;
+            }
+            if(nums[mid] > target){
+                high = mid;
+            }
+            if(nums[mid] < target){
+                low = mid +1;
+            }
+        }
+        return -1;
 
+    }
 }
