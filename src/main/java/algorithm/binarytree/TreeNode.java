d1cd1507150df01e4f5bd1f41a6f94c6cdb45019
xiaoran <ran.xiao@tendcloud.com>
2020-06-29 15:30:14 +0800

    treenode设置为都可以访问使用

--- src/main/java/algorithm/binarytree/TreeNode.java
+++ src/main/java/algorithm/binarytree/TreeNode.java
@@ -6,9 +6,9 @@ package algorithm.binarytree;
  */
 public class TreeNode {
 
-    int val;
-    TreeNode left;
-    TreeNode right;
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
 
     TreeNode(int x) {
         val = x;
